5ca9f3d96ad1a3df4c286f20fe023147b3727495
Fabrice Daugan <fabrice.daugan@gmail.com>
2019-07-23 12:10:05 +0200

    Lint deprecation

--- src/main/resources/META-INF/resources/webjars/service/scm/scm.js
+++ src/main/resources/META-INF/resources/webjars/service/scm/scm.js
@@ -20,7 +20,7 @@ define(function () {
 		renderFeaturesScm: function (subscription, type) {
 			// Add URL link
 			var url = current.$super('getData')(subscription, 'service:scm:' + type + ':url') + '/' + current.$super('getData')(subscription, 'service:scm:' + type + ':repository');
-			var result = current.$super('renderServicelink')('home', url, 'service:scm:' + type + ':repository', null, 'target="_blank"');
+			var result = current.$super('renderServiceLink')('home', url, 'service:scm:' + type + ':repository', null, 'target="_blank"');
 
 			// Add Copy URL
 			result += '<button class="btn-link service-scm-clipboard" data-toggle="tooltip" title="' + current.$messages['copy-clipboard'] + '" data-container="body"><i class="far fa-clipboard"></i></button>';
